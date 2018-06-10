6c69a46c0f57da5e38322cc315ca5d227511c66e
wzh <1009070047@qq.com>
2018-06-10 15:27:20 +0800

    2

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/MavendockerplugindemoApplication.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/MavendockerplugindemoApplication.java
@@ -2,10 +2,16 @@ package com.bolingcavalry.mavendockerplugindemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MavendockerplugindemoApplication {
+public class MavendockerplugindemoApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MavendockerplugindemoApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MavendockerplugindemoApplication.class, args);
 	}
