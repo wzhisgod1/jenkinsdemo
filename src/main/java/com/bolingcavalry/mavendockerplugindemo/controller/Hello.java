636ff37def9b3c5bedf21403a7bba1a5ea2c17e1
wzh <1009070047@qq.com>
2018-06-10 17:43:34 +0800

    3

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -11,6 +11,6 @@ public class Hello {
     @RequestMapping("/")
     public String sayHello(){
     	//修改返回的字符串的内容
-        return "12345666777sdfsdf. Hello jenkins, " + new Date();
+        return "12345666777bb. Hello jenkins, " + new Date();
     }
 }
